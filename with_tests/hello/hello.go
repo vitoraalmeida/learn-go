5de169fbe6429647715f4edd9e4a026578847bff
Vitor Almeida <pro.vitoralmeida@gmail.com>
2022-06-23 11:35:23 -0300

    Add explanations about the code

--- with_tests/hello/hello.go
+++ with_tests/hello/hello.go
@@ -1,37 +1,57 @@
+// Arquivos que contem testes devem seguir o padrão XXX_test.go
 package main
 
-import "fmt"
+// o pacote padrão para realização de testes
+import "testing"
 
-const spanish = "Spanish"
-const french = "French"
-const english = "English"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-const portugueseHelloPrefix = "Olá, "
-
-func greetingPrefix(language string) (prefix string) {
-    switch language {
-    case spanish:
-        prefix = spanishHelloPrefix
-    case french:
-        prefix = frenchHelloPrefix
-    case english:
-        prefix = englishHelloPrefix
-    default:
-        prefix = portugueseHelloPrefix
-    }
-    return
-}
-
-func Hello(name, language string) string {
-    if name == "" {
-        name = "Mundo"
+// funções que executam testes devem seguir o padrão TestNomeDaFuncaoTestada
+// e receber como parâmetro um ponteiro para o tipo testing.T, que possui as 
+// funcionalidades de testes
+func TestHello(t *testing.T) {
+    // função auxiliar de teste criada quando o teste específico se repetirá
+    // assertCorrectMessage está armazenando uma função anônima (quando não declarada com func)
+    // o tipo testing.TB é um tipo que permite tanto testes comuns (T) quando banckmark (B)
+    assertCorrectMessage := func(t testing.TB, got, want string) {
+        // marcar o test como helper permite que, se o teste falhar, a saída tenha
+        // informação da linha específica do teste que falhou. Caso contrário,
+        // será a linha da definição do helper
+        t.Helper()
+        if got != want {
+            // define um caso de falha para o teste e a mensagem que deve ser exibida
+            // %q formata o valor com aspas duplas
+            t.Errorf("got %q want %q", got, want)
+        }
     }
 
-    return greetingPrefix(language) + name
-}
+    // t.Run("Nome do caso de teste", funçãoQueDefineOTesteExecutado)
+    t.Run("saying Hello to people", func(t *testing.T) {
+        got := Hello("Xibungo", "")
+        want := "Olá, Xibungo"
+        // uso do helper em cada caso de teste, assim fica mais legível e limpo
+        assertCorrectMessage(t, got, want)
+    })
 
-func main() {
-    fmt.Println(Hello("Batata", ""))
+    t.Run("say 'Olá, mundo' when an empty string is supplied", func(t *testing.T){
+        got := Hello("", "")
+        want := "Olá, Mundo"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in Spanish", func(t *testing.T) {
+        got := Hello("Elodie", "Spanish")
+        want := "Hola, Elodie"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in French", func(t *testing.T) {
+        got := Hello("Joana", "French")
+        want := "Bonjour, Joana"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in English", func(t *testing.T){
+        got := Hello("Frank", "English")
+        want := "Hello, Frank"
+        assertCorrectMessage(t, got, want)
+    })
 }
--- with_tests/hello/hello_test.go
+++ with_tests/hello/hello_test.go
@@ -1,42 +1,70 @@
+// define que este arquivo pertence a um pacote
+// um pacote agrega código que estão sob o mesmo contexto
 package main
 
-import "testing"
+// importa um pacote
+// fmt é um pacote que pertence a biblioteca padrão
+import "fmt"
 
-func TestHello(t *testing.T) {
-    assertCorrectMessage := func(t testing.TB, got, want string) {
-        t.Helper()
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
+// constantes inserem o valor no "texto" do binário, então
+// não memória auxiliar não é alocada para salvar o valor,
+// por isso são mais econômicas em uso de memória
+const spanish = "Spanish" // strings são definidas com " "
+const french = "French"
+const english = "English"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const portugueseHelloPrefix = "Olá, "
+
+// func declara uma nova função
+// greetingPrefix é o nome da função
+// (language string) é o parâmetro que a função espera
+// para excecutar seu trabalho, sendo que "language" é o nome
+// da variável que armazenará o valor passado quando ela for chamada
+// e "string" declara o tipo do argumento
+// (prefix string) é o valor de retorno da função
+// quando o retorno é nomeado, a função terá acesso à variável
+// que foi declarada como retorno, sendo que ela terá o valor padrão para o tipo
+// definido
+// func nome(argumento tipoDoArgumento) (retorno tipoDoRetorno)
+func greetingPrefix(language string) (prefix string) {
+    // switch é uma estrutura de controle que compara um valor
+    // e se esse valor for igual ao das opções definidas, o código
+    // da opção será executado
+    switch language {
+    case spanish:
+        prefix = spanishHelloPrefix
+    case french:
+        prefix = frenchHelloPrefix
+    case english:
+        prefix = englishHelloPrefix
+    // default é o caso que será executado caso nenhuma das opções bater com o 
+    // valor comparado
+    default:
+        prefix = portugueseHelloPrefix
     }
+    return // quando o valor de retorno é nomeado na assinatura da função,
+    // é ele que será retornado caso retorn estiver sozinho
+}
 
-    t.Run("saying Hello to people", func(t *testing.T) {
-        got := Hello("Xibungo", "")
-        want := "Olá, Xibungo"
-        assertCorrectMessage(t, got, want)
-    })
+// funções que começam com letra maiúscula tem visibilidade entre pacotes
+// em minúsculo possuem visibilidade apenas dentro do pacote
+func Hello(name, language string) string {
+    if name == "" {
+        name = "Mundo"
+    }
+    // é possivel concatenar string usando a operação de soma
+    return greetingPrefix(language) + name
+}
 
-    t.Run("say 'Olá, mundo' when an empty string is supplied", func(t *testing.T){
-        got := Hello("", "")
-        want := "Olá, Mundo"
-        assertCorrectMessage(t, got, want)
-    })
-
-    t.Run("in Spanish", func(t *testing.T) {
-        got := Hello("Elodie", "Spanish")
-        want := "Hola, Elodie"
-        assertCorrectMessage(t, got, want)
-    })
-
-    t.Run("in French", func(t *testing.T) {
-        got := Hello("Joana", "French")
-        want := "Bonjour, Joana"
-        assertCorrectMessage(t, got, want)
-    })
-
-    t.Run("in English", func(t *testing.T){
-        got := Hello("Frank", "English")
-        want := "Hello, Frank"
-        assertCorrectMessage(t, got, want)
-    })
+func main() {
+    // fmt.Println() imprime valores na saída padrão
+    // nesse caso, foi passada uma função para ela, pois é importante isolar a regra
+    // de negócio (o problema que está sendo resolvido) de fatores externos a ela
+    // (imprimir o valor no terminal). Poderiamos retornar o valor da função Hello
+    // para qualquer tipo de saída que aceite texto (arquivo, resposta http etc)
+    // isso permite que o código seja mais testável, inclusive, já que ele em si
+    // tem menos dependências
+    fmt.Println(Hello("Batata", ""))
 }
