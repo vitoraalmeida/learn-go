b5b15acef2b23795c6e5213024582c528034298e
Vitor <pro.vitoralmeida@gmail.com>
2023-03-27 19:51:19 -0300

    Adiciona um tipo customizado para as flags C e N. Modifica main para que seja testável. Adiciona testes para a CLI.

--- hit/cmd/hit/flags.go
+++ hit/cmd/hit/flags.go
@@ -6,15 +6,22 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 )
+
+// Mensagem adicional de uso
+const usageText = `
+Usage:
+  hit [options] url
+Options:`
 
 type flags struct {
 	url  string
 	n, c int //n = número de requests | c = nível de concorrência
 }
 
-func (f *flags) parse() (err error) {
+func (f *flags) parse(s *flag.FlagSet, args []string) (err error) {
 	/* comentado pois passando diretamente os campos do struct que já temos ao invés de salvar na variável interna do flag
 
 		var (
@@ -42,13 +49,28 @@ func (f *flags) parse() (err error) {
 	f.n = *n
 	f.c = *c
 	*/
-	flag.StringVar(&f.url, "url", "", "HTTP server `URL` to make requests (required)")
-	flag.IntVar(&f.n, "n", f.n, "Number of requests to make")
-	flag.IntVar(&f.c, "c", f.c, "Concurrency level")
-	flag.Parse()
+
+	s.Usage = func() {
+		fmt.Fprintln(os.Stderr, usageText[1:])
+		s.PrintDefaults()
+	}
+	// usando como positional argument -> tem que ser por último
+	//flag.StringVar(&f.url, "url", "", "HTTP server `URL` to make requests (required)")
+
+	// utlizando um tipo customizado que só permite inteiros positivos
+	s.Var(toNumber(&f.n), "n", "Number of requests to make")
+	s.Var(toNumber(&f.c), "c", "Concurrency level")
+	if err := s.Parse(args); err != nil {
+		return err
+	}
+
+	// o flag parseia as flags definidas e o que não estiver definido anteriormente
+	// o flag ira adicionar numa lista. Se não houver, uma string vazia é retornada
+	f.url = s.Arg(0)
+
 	if err := f.validate(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		flag.Usage()
+		fmt.Fprintln(s.Output(), err)
+		s.Usage()
 		return err
 	}
 	return nil
@@ -61,7 +83,8 @@ func (f *flags) validate() error {
 		return fmt.Errorf("-c=%d: should be less than or equal to -n=%d", f.c, f.n)
 	}
 	if err := validateURL(f.url); err != nil {
-		return fmt.Errorf("invalid value %q for flag -url: %w", f.url, err)
+		//return fmt.Errorf("invalid value %q for flag -url: %w", f.url, err)
+		return fmt.Errorf("url: %w", err)
 	}
 	return nil
 }
@@ -80,6 +103,30 @@ func validateURL(s string) error {
 	}
 	// TrimSpace remove todos espaços ao redor
 	return err
+}
+
+// number é um número natural
+type number int
+
+func toNumber(p *int) *number {
+	return (*number)(p) // converte um *int para um *number, incluindo os métodos
+	// de number a *int
+}
+
+func (n *number) Set(s string) error {
+	v, err := strconv.ParseInt(s, 0, strconv.IntSize)
+	switch {
+	case err != nil:
+		err = errors.New("parse error")
+	case v <= 0:
+		err = errors.New("should be positive")
+	}
+	*n = number(v)
+	return err
+}
+
+func (n *number) String() string {
+	return strconv.Itoa(int(*n))
 }
 
 /*
--- hit/cmd/hit/hit.go
+++ hit/cmd/hit/hit.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"runtime"
 )
@@ -31,16 +33,25 @@ func banner() string { return bannerText[1:] } //remove o primeiro \n
 //func usage() string  { return usageText[1:] }  //remove o primeiro \n
 
 func main() {
+	if err := run(flag.CommandLine, os.Args[1:], os.Stdout); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(s *flag.FlagSet, args []string, out io.Writer) error {
 	// padrão de execução caso não seja preenchido pelo usuário
 	f := &flags{
 		n: 100,
 		c: runtime.NumCPU(),
 	}
 
-	if err := f.parse(); err != nil {
-		os.Exit(1)
+	if err := f.parse(s, args); err != nil {
+		return err
 	}
-	fmt.Println(banner())
-	fmt.Printf("Making %d requests to %s with a concurrency level of %d.\n",
+
+	fmt.Fprintln(out, banner())
+	fmt.Fprintf(out, "Making %d requests to %s with a concurrency level of %d.\n",
 		f.n, f.url, f.c)
+
+	return nil
 }
